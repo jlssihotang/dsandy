21d58bb53643c2ed405d34a3fdc4e053a6f36e23
jlssihotang <69977498+jlssihotang@users.noreply.github.com>
2020-08-26 23:59:04 +0700

    f

--- main.js
+++ main.js
@@ -14,5 +14,75 @@ $(document).ready(function() {
         });
        });
     
+    $("#btn-1").click(function() {
+        $('.container').animate({
+            scrollTop: $("#sec-3").offset().top
+        }, 1000);
+        $("#sec-3-01").removeClass("hide");
+        $('#sec-3-01').click(function() {
+        $("#sec-3-01").addClass("animate__zoomOut").delay(1000).queue(function(next){
+            next();
+            $('#sec-3-02').removeClass('hide');
+        });
+        console.log('foo');
+       });
+        $('#sec-3-02').click(function() {
+            $("#sec-3-02").addClass("animate__zoomOut").delay(1000).queue(function(next){
+                next();
+                $('#sec-3-03').removeClass('hide');
+            });
+        });
+    });
+
+    // $("#btn-2").click(function() {
+    //     $('.container').animate({
+    //         scrollTop: $("#sec-3").offset().top
+    //     }, 1000);
+    //     console.log('btn2');
+    //     $("#sec-3-01").removeClass("hide");
+    //     $('#sec-3-01').click(function() {
+    //     $("#sec-3-01").addClass("animate__zoomOut").delay(1000).queue(function(next){
+    //         next();
+    //         $('#sec-3-02').removeClass('hide');
+    //     });
+    //     console.log('foo');
+    //    });
+    //     $('#sec-3-02').click(function() {
+    //         $("#sec-3-02").addClass("animate__zoomOut").delay(1000).queue(function(next){
+    //             next();
+    //             $('#sec-3-03').removeClass('hide');
+    //         });
+    //     });
+    // });
+
+    $("#btn-3").click(function() {
+        $('.container').animate({
+            scrollTop: $("#sec-5").offset().top
+        }, 800);
+        console.log('asd');
+        $("#sec-4-01").removeClass("hide");
+        $('#sec-4-01').click(function() {
+        $("#sec-4-01").addClass("animate__zoomOut").delay(1000).queue(function(next){
+            next();
+            $('#sec-4-02').removeClass('hide');
+        });
+        console.log('foo');
+       });
+        $('#sec-4-02').click(function() {
+            $("#sec-4-02").addClass("animate__zoomOut").delay(1000).queue(function(next){
+                next();
+                $('#sec-4-03').removeClass('hide');
+            });
+        });
+    });
+
+    $("#btn-4").click(function() {
+        $(".container").animate({ scrollTop: $('.container').prop("scrollHeight")}, 1000);
+        console.log('asd');
+        $("#sec-5-01").removeClass("hide");
+    });
+
+    
+    
 });
 
